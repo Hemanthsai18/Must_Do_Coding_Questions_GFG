e62540c7792d11260712515859cbda530cb642b5
190030318_Hemanth <67505856+Hemanthsai18@users.noreply.github.com>
2021-11-21 12:16:35 +0530

    Create K-th_element_of_two_sorted_Arrays.cpp
    Arrays

---
+++ K-th_element_of_two_sorted_Arrays.cpp
@@ -0,0 +1,35 @@
+class Solution{
+public:
+    int kthElement(int a[], int b[], int n, int m, int k)
+    {
+        int t=0,ans=0;
+        int i=0,j=0;
+        while(i<n&&j<m){
+            if(a[i]<b[j]){
+                ans=a[i];
+                i++;
+                
+            }
+            else{
+                ans=b[j];
+                j++;
+            }
+            t++;
+            if(t==k)
+            return ans;
+        }
+        while(i<n){
+            t++;
+            if(t==k)
+            return a[i];
+            i++;
+        }
+        while(j<m){
+            t++;
+            if(t==k)
+            return b[j];
+            j++;
+        }
+        
+    }
+};
