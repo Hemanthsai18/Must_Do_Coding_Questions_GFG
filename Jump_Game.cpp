32cdf3bb8b55b33783c4e6298cb607ca7295cedf
190030318_Hemanth <67505856+Hemanthsai18@users.noreply.github.com>
2021-11-21 12:13:03 +0530

    Create Jump_Game.cpp

---
+++ Jump_Game.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+  public:
+    int canReach(int a[], int n) {
+        int reach=0;
+        for(int i=0;i<n;i++){
+            if(reach<i)
+            return 0;
+            if((i+a[i])>reach)
+            reach=i+a[i];
+        }
+        if(reach>=n-1)
+        return 1;
+        return 0;
+        // code here
+    }
+};
